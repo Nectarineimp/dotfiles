fbca4820f88afc33aa6e9da31a02b1d8bbcc7ba8
calebphillips <caleb.phillips@gmail.com>
2012-12-04 15:48:19 -0600

    Add storm stuff and get rid of sonian stuff

--- init.el
+++ init.el
@@ -1,6 +1,8 @@
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
 (when (not package-archive-contents)
@@ -20,8 +22,9 @@
                       tuareg
                       marmalade
                       oddmuse
-                      scpaste
-                      paredit)
+                      paredit
+                      molokai-theme
+                      nrepl)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
@@ -30,74 +33,18 @@
 
 (defun turn-on-paredit () (paredit-mode 1))
 (add-hook 'clojure-mode-hook 'turn-on-paredit)
+
+(defun turn-on-rainbow () (rainbow-delimiters-mode 1))
+(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+
 (add-hook 'slime-repl-mode-hook 'clojure-mode-font-lock-setup)
 
-(set-frame-font "Menlo-14")
+(set-frame-font "Inconsolata-16")
 
-;; I like monokai but it was wonky on tmux
-(color-theme-monokai)
-;;(load-theme 'wombat)
+(color-theme-molokai)
 
 (add-to-list 'auto-mode-alist '("\.cljs$" . clojure-mode))
 (add-to-list 'auto-mode-alist '("\.md$" . markdown-mode))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; Allow input to be sent to somewhere other than inferior-lisp
-;; This all comes from the clojurescript one wiki
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; This is a total hack: we're hardcoding the name of the shell buffer
-(defun shell-send-input (input)
-  "Send INPUT into the *shell* buffer and leave it visible."
-  (save-selected-window
-    (switch-to-buffer-other-window "*shell*")
-    (goto-char (point-max))
-    (insert input)
-    (comint-send-input)))
-
-(defun defun-at-point ()
-  "Return the text of the defun at point."
-  (apply #'buffer-substring-no-properties
-         (region-for-defun-at-point)))
-
-(defun region-for-defun-at-point ()
-  "Return the start and end position of defun at point."
-  (save-excursion
-    (save-match-data
-      (end-of-defun)
-      (let ((end (point)))
-        (beginning-of-defun)
-        (list (point) end)))))
-
-(defun expression-preceding-point ()
-  "Return the expression preceding point as a string."
-  (buffer-substring-no-properties
-   (save-excursion (backward-sexp) (point))
-   (point)))
-
-(defun shell-eval-last-expression ()
-  "Send the expression preceding point to the *shell* buffer."
-  (interactive)
-  (shell-send-input (expression-preceding-point)))
-
-(defun shell-eval-defun ()
-  "Send the current toplevel expression to the *shell* buffer."
-  (interactive)
-  (shell-send-input (defun-at-point)))
-
-(add-hook 'clojure-mode-hook
-          '(lambda ()
-             (define-key clojure-mode-map (kbd "C-c e") 'shell-eval-last-expression)
-             (define-key clojure-mode-map (kbd "C-c x") 'shell-eval-defun)))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(inferior-lisp-program "lein2 repl"))
-
 
 (require 'server)
 (unless (server-running-p)
